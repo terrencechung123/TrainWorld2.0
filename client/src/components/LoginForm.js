5a51f3f65acdd5f34b9fbe2004ab50a7ce7147ea
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-03-28 15:55:24 -0600

    login formik done with error handling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -11,35 +11,35 @@ const CustomLabel = styled.label`
 `;
 
 function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [errors, setErrors] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setIsLoading(true);
-    fetch("/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    }).then((r) => {
-      setIsLoading(false);
-      if (r.ok) {
-        r.json().then((user) => onLogin(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });
-  }
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     setIsLoading(true);
+//     fetch("/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username, password }),
+//     }).then((r) => {
+//       setIsLoading(false);
+//       if (r.ok) {
+//         r.json().then((user) => onLogin(user));
+//       } else {
+//         r.json().then((err) => setErrors(err.errors));
+//       }
+//     });
+//   }
 
 
   const validationSchema = yup.object({
     username: yup.string().required(),
     password: yup.string().required(),
-  })
+  });
 
 
   const formik = useFormik({
@@ -47,7 +47,6 @@ function LoginForm({ onLogin }) {
       username: "",
       password: "",
     },
-
     validationSchema,
     onSubmit: (values, { setErrors, setSubmitting }) => {
       setSubmitting(true);
@@ -61,28 +60,29 @@ function LoginForm({ onLogin }) {
       .then((r) => {
         setSubmitting(false);
         if (r.ok) {
-          r.json.then((user) => onLogin(user))
+          r.json().then((user) => onLogin(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
         }
       })
-    }
-  })
-
-
-
-
-
+      .catch((error) => {
+        setSubmitting(false);
+        console.log(`Error: ${error}`)
+      });
+    },
+  });
 
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <FormField>
         <CustomLabel htmlFor="username">Username</CustomLabel>
         <Input
           type="text"
           id="username"
           autoComplete="off"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />
       </FormField>
       <FormField>
@@ -91,17 +91,18 @@ function LoginForm({ onLogin }) {
           type="password"
           id="password"
           autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
       </FormField>
       <FormField>
         <Button variant="fill" color="primary" type="submit">
-          {isLoading ? "Loading..." : "Login"}
+          {formik.isSubmitting ? "Loading..." : "Login"}
         </Button>
       </FormField>
       <FormField>
-        {errors.map((err) => (
+        {formik.errors &&
+        Object.values(formik.errors).map((err) => (
           <Error key={err}>{err}</Error>
         ))}
       </FormField>
